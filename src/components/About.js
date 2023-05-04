636991358e6b1914cf845dd756c52cb4b7baef51
Eduardo Heyer <eduardo.heyer12@gmail.com>
2023-05-04 11:36:34 -0400

    completed react lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,26 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About({bio, links}) {
+ 
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <div> 
+        {bio ? <p>{bio}</p> : null}
+      </div>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links linkedin={links.linkedin} github={links.github} />
     </div>
   );
 }
+
+// function Bio({bio}){
+
+//   if (bio) {
+//     return <p>{bio}</p>
+//   }
+//   return <></>
+// }
 
 export default About;
